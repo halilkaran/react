caa1e2120716fc7c49f3bbc8f09a28f633c37bff
Halil <78084765+halilkaran@users.noreply.github.com>
2022-03-08 08:34:09 +0100

    first commit

---
+++ class-notes/S01_Install_Starter/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+//* render method gets two parameters
+//* 1-) the component to be rendered.
+//* 2-) the component to be rendered in which DOM element
+
+ReactDOM.render(<App />, document.getElementById("root"));
